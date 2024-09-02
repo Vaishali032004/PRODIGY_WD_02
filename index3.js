2817118a8856984ffd6ecbe3ffc601f8b73613e7
Vaishali032004 <vaishalikotari05@gmail.com>
2024-09-02 19:24:39 +0530

    stopwatch web application

---
+++ index3.js
@@ -0,0 +1,44 @@
+/* styles.css */
+body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: #f4f4f4;
+    margin: 0;
+}
+
+.stopwatch {
+    text-align: center;
+    font-family: Arial, sans-serif;
+    background-color: rgb(92, 194, 241);
+}
+
+.display {
+    font-size: 48px;
+    margin-bottom: 20px;
+}
+
+.controls {
+    margin-bottom: 20px;
+}
+
+button {
+    padding: 10px 20px;
+    font-size: 16px;
+    margin: 0 5px;
+    cursor: pointer;
+    background-color: azure;
+}
+
+.laps {
+    margin-top: 20px;
+    text-align: left;
+}
+
+.lap {
+    background-color: #e98d8d;
+    border: 1px solid #ddd;
+    padding: 10px;
+    margin: 5px 0;
+}
